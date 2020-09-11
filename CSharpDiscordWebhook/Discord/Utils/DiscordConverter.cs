9e688870c08e54ad69029eb7eb5ec7a9c96b9a03
NatanM <natanaelmarian@hotmail.com>
2020-09-11 15:18:31 -0300

    Added to help users converting things like System.Drawing.Colors to Integer

---
+++ CSharpDiscordWebhook/Discord/Utils/DiscordConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace Discord.Utils
+{
+    public class DiscordConverter
+    {
+        public static int ColorToHex(Color SourceColor)
+        {
+            string HexString = SourceColor.R.ToString("X2") + 
+                SourceColor.G.ToString("X2") + 
+                SourceColor.B.ToString("X2");
+
+            return int.Parse(HexString, System.Globalization.NumberStyles.HexNumber); 
+        }
+    }
+}
