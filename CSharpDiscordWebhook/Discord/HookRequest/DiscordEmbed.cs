249da57220d02fdca4854d367eb39de8332a4355
NatanM <natanaelmarian@hotmail.com>
2020-09-11 18:39:00 -0300

    Added embed fields support

--- CSharpDiscordWebhook/Discord/HookRequest/DiscordEmbed.cs
+++ CSharpDiscordWebhook/Discord/HookRequest/DiscordEmbed.cs
@@ -4,10 +4,20 @@ namespace Discord.Webhook.HookRequest
 {
     public class DiscordEmbed
     {
+        /// <summary>
+        /// Create discord embed
+        /// </summary>
+        /// <param name="Title">Embed title</param>
+        /// <param name="Description">Embed description</param>
+        /// <param name="Color">Embed color</param>
+        /// <param name="ImageUrl">Embed image</param>
+        /// <param name="FooterText">Embed footer text</param>
+        /// <param name="FooterIconUrl">Embed footer icon</param>
+        /// <param name="Fields">Embed fields</param>
         public DiscordEmbed(string Title="", string Description="", 
-            int Color=0, string ImageUrl="", string FooterText="", string FooterIconUrl="")
+            int Color=0, string ImageUrl="", string FooterText="", string FooterIconUrl="",
+            DiscordEmbedField[] Fields=null)
         {
-
             var embedData = new JObject();
             embedData.Add("title", Title);
             embedData.Add("description", Description);
@@ -23,9 +33,19 @@ namespace Discord.Webhook.HookRequest
             embedData.Add("image", imageData);
             embedData.Add("footer", footerData);
 
+            if(Fields != null)
+            {
+                var fieldsData = new JArray();
+                foreach (DiscordEmbedField field in Fields)
+                    fieldsData.Add(field.JsonData);
+                embedData.Add("fields", fieldsData);
+            }
             JsonData = embedData;
         }
 
+        /// <summary>
+        /// Get embed request json
+        /// </summary>
         public JObject JsonData { get; private set; }
     }
 }
---
+++ CSharpDiscordWebhook/Discord/HookRequest/DiscordEmbedField.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord.Webhook.HookRequest
+{
+    public class DiscordEmbedField
+    {
+        /// <summary>
+        /// Create embed field object
+        /// </summary>
+        /// <param name="Name">Embed name</param>
+        /// <param name="Value">Embed value</param>
+        /// <param name="Line">True => Embed fields will go from left to right.
+        /// False => Embed fields will go from top to down.</param>
+        public DiscordEmbedField(string Name, string Value, bool Line=true)
+        {
+            JObject fieldData = new JObject();
+            fieldData.Add("name", Name);
+            fieldData.Add("value", Value);
+            fieldData.Add("inline", Line);
+
+            JsonData = fieldData;
+        }
+
+        public JObject JsonData { get; private set; }
+    }
+}
