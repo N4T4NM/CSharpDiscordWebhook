6ce5845926882cee0aa672b8dc8ec4cec987f2f4
NatanM <natanaelmarian@hotmail.com>
2020-09-11 15:11:21 -0300

    New code for embeds, do the same thing but with less lines

---
+++ CSharpDiscordWebhook/Discord/HookRequest/DiscordEmbed.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Discord.Webhook.HookRequest
+{
+    public class DiscordEmbed
+    {
+        public DiscordEmbed(string Title="", string Description="", 
+            int Color=0, string ImageUrl="", string FooterText="", string FooterIconUrl="")
+        {
+
+            var embedData = new JObject();
+            embedData.Add("title", Title);
+            embedData.Add("description", Description);
+            embedData.Add("color", Color);
+
+            var imageData = new JObject();
+            imageData.Add("url", ImageUrl);
+
+            var footerData = new JObject();
+            footerData.Add("text", FooterText);
+            footerData.Add("icon_url", FooterIconUrl);
+
+            embedData.Add("image", imageData);
+            embedData.Add("footer", footerData);
+
+            JsonData = embedData;
+        }
+
+        public JObject JsonData { get; private set; }
+    }
+}
