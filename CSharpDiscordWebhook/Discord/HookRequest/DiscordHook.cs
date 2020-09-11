53f32b6351f480211e68a8f4c0514db1874cd33b
NatanM <natanaelmarian@hotmail.com>
2020-09-11 15:16:10 -0300

    Added DiscordHook class to store the message content

---
+++ CSharpDiscordWebhook/Discord/HookRequest/DiscordHook.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Discord.Webhook.HookRequest
+{
+    public class DiscordHook
+    {
+        internal DiscordHook(MemoryStream BodyData, string Bound)
+        {
+            this.Body = BodyData;
+            this.Boundary = Bound;
+        }
+
+        public string Boundary { get; private set; }
+        public MemoryStream Body { get; private set; }
+    }
+}
