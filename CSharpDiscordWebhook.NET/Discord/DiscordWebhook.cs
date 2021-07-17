bc217a39ffc6556fa38d86d31d91be4887edec5f
NatanM <natanaelmarian@hotmail.com>
2021-07-17 11:19:07 -0300

    Added a .NET 5 version

---
+++ CSharpDiscordWebhook.NET/Discord/DiscordWebhook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace CSharpDiscordWebhook.NET.Discord
+{
+    public class DiscordWebhook
+    {
+        /// <summary>
+        /// Webhook url
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Send webhook message
+        /// </summary>
+        public void Send(DiscordMessage message, FileInfo file = null)
+        {
+            if (string.IsNullOrEmpty(Url))
+                throw new ArgumentNullException("Invalid Webhook URL.");
+
+            string bound = "------------------------" + DateTime.Now.Ticks.ToString("x");
+
+            WebClient webhook = new WebClient();
+            webhook.Headers.Add("Content-Type", "multipart/form-data; boundary=" + bound);
+
+            MemoryStream stream = new MemoryStream();
+            stream.Write($"--{bound}\r\n");
+
+            if (file != null && file.Exists)
+            {
+                string fileBegin = $"Content-Disposition: form-data; name=\"file\"; filename=\"{file.Name}\"\r\n";
+                string fileContentType = "Content-Type: application/octet-stream\r\n\r\n";
+                string fileEnd = $"\r\n--{bound}\r\n";
+
+                stream.Write(fileBegin);
+                stream.Write(fileContentType);
+                stream.Write(File.ReadAllBytes(file.FullName));
+                stream.Write(fileEnd);
+            }
+
+            string jsonBegin = "Content-Disposition: form-data; name=\"payload_json\"\r\n";
+            string jsonContentType = "Content-Type: application/json\r\n\r\n";
+            string jsonBody = JsonSerializer.Serialize<DiscordMessage>(message);
+            string jsonEnd = $"\r\n--{bound}--";
+
+            stream.Write(jsonBegin);
+            stream.Write(jsonContentType);
+            stream.Write(jsonBody);
+            stream.Write(jsonEnd);
+
+            try
+            {
+                webhook.UploadData(Url, stream.ToArray());
+            } catch(WebException ex)
+            {
+                throw new InvalidOperationException(ex.Response.GetResponseStream().Decode());
+            }
+            stream.Dispose();
+        }
+    }
+}
---
+++ CSharpDiscordWebhook.NET/Discord/Extensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace CSharpDiscordWebhook.NET.Discord
+{
+    public static class Extensions
+    {
+        public static byte[] Encode(this string source)
+            => Encoding.UTF8.GetBytes(source);
+        public static string Decode(this byte[] source)
+            => Encoding.UTF8.GetString(source);
+
+        public static void Write(this MemoryStream source, string str)
+        {
+            byte[] buffer = str.Encode();
+            source.Write(buffer, 0, buffer.Length);
+        }
+
+        public static int? ToHex(this Color? color)
+        {
+            string HS =
+                color?.R.ToString("X2") +
+                color?.G.ToString("X2") +
+                color?.B.ToString("X2");
+
+            int hex;
+            if (int.TryParse(HS, System.Globalization.NumberStyles.HexNumber, null, out hex))
+                return hex;
+            else return null;
+        }
+
+        public static Color? ToColor(this int? hex)
+        {
+            if (hex == null)
+                return null;
+
+            return ColorTranslator.FromHtml(hex?.ToString("X6"));
+        }
+
+        public static string Decode(this Stream source)
+        {
+            using (StreamReader reader = new StreamReader(source))
+                return reader.ReadToEnd();
+        }
+    }
+}
