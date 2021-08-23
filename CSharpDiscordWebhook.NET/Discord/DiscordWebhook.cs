ebf87f223eee9f1257548bd436a54000598577e0
NatanM <natanaelmarian@hotmail.com>
2021-08-23 13:48:49 -0300

    Added support for multiple files

--- CSharpDiscordWebhook.NET/Discord/DiscordWebhook.cs
+++ CSharpDiscordWebhook.NET/Discord/DiscordWebhook.cs
@@ -12,10 +12,35 @@ namespace Discord.NET.Webhook
         /// </summary>
         public string Url { get; set; }
 
+        private void AddField(MemoryStream stream, string bound, string cDisposition, string cType, byte[] data)
+        {
+            string prefix = stream.Length > 0 ? "\r\n--" : "--";
+            string fBegin = $"{prefix}{bound}\r\n";
+
+            stream.Write(fBegin);
+            stream.Write(cDisposition);
+            stream.Write(cType);
+            stream.Write(data);
+        }
+
+        private void SetJsonPayload(MemoryStream stream, string bound, string json)
+        {
+            string cDisposition = "Content-Disposition: form-data; name=\"payload_json\"\r\n";
+            string cType = "Content-Type: application/octet-stream\r\n\r\n";
+            AddField(stream, bound, cDisposition, cType, json.Encode());
+        }
+
+        private void SetFile(MemoryStream stream, string bound, int index, FileInfo file)
+        {
+            string cDisposition = $"Content-Disposition: form-data; name=\"file_{index}\"; filename=\"{file.Name}\"\r\n";
+            string cType = "Content-Type: application/octet-stream\r\n\r\n";
+            AddField(stream, bound, cDisposition, cType, File.ReadAllBytes(file.FullName));
+        }
+
         /// <summary>
         /// Send webhook message
         /// </summary>
-        public void Send(DiscordMessage message, FileInfo file = null)
+        public void Send(DiscordMessage message, params FileInfo[] files)
         {
             if (string.IsNullOrEmpty(Url))
                 throw new ArgumentNullException("Invalid Webhook URL.");
@@ -26,36 +51,19 @@ namespace Discord.NET.Webhook
             webhook.Headers.Add("Content-Type", "multipart/form-data; boundary=" + bound);
 
             MemoryStream stream = new MemoryStream();
-            stream.Write($"--{bound}\r\n");
+            for(int i=0; i < files.Length; i++)
+                SetFile(stream, bound, i, files[i]);
 
-            if (file != null && file.Exists)
-            {
-                string fileBegin = $"Content-Disposition: form-data; name=\"file\"; filename=\"{file.Name}\"\r\n";
-                string fileContentType = "Content-Type: application/octet-stream\r\n\r\n";
-                string fileEnd = $"\r\n--{bound}\r\n";
-
-                stream.Write(fileBegin);
-                stream.Write(fileContentType);
-                stream.Write(File.ReadAllBytes(file.FullName));
-                stream.Write(fileEnd);
-            }
-
-            string jsonBegin = "Content-Disposition: form-data; name=\"payload_json\"\r\n";
-            string jsonContentType = "Content-Type: application/json\r\n\r\n";
-            string jsonBody = JsonSerializer.Serialize<DiscordMessage>(message);
-            string jsonEnd = $"\r\n--{bound}--";
-
-            stream.Write(jsonBegin);
-            stream.Write(jsonContentType);
-            stream.Write(jsonBody);
-            stream.Write(jsonEnd);
+            string json = JsonSerializer.Serialize(message);
+            SetJsonPayload(stream, bound, json);
+            stream.Write($"\r\n--{bound}--");
 
             try
             {
                 webhook.UploadData(Url, stream.ToArray());
             } catch(WebException ex)
             {
-                throw new InvalidOperationException(ex.Response.GetResponseStream().Decode());
+                throw new WebException(ex.Response.GetResponseStream().Decode());
             }
             stream.Dispose();
         }
--- CSharpDiscordWebhook/Discord/DiscordWebHook.cs
+++ CSharpDiscordWebhook/Discord/DiscordWebHook.cs
@@ -12,10 +12,39 @@ namespace Discord.Webhook
         /// </summary>
         public string Url { get; set; }
 
+        private void AddField(MemoryStream stream, string bound, string cDisposition, string cType, byte[] data)
+        {
+            string prefix = stream.Length > 0 ? "\r\n--" : "--";
+            string fBegin = $"{prefix}{bound}\r\n";
+
+            byte[] fBeginBuffer = Utils.Encode(fBegin);
+            byte[] cDispositionBuffer = Utils.Encode(cDisposition);
+            byte[] cTypeBuffer = Utils.Encode(cType);
+
+            stream.Write(fBeginBuffer, 0, fBeginBuffer.Length);
+            stream.Write(cDispositionBuffer, 0, cDispositionBuffer.Length);
+            stream.Write(cTypeBuffer, 0, cTypeBuffer.Length);
+            stream.Write(data, 0, data.Length);
+        }
+
+        private void SetJsonPayload(MemoryStream stream, string bound, string json)
+        {
+            string cDisposition = "Content-Disposition: form-data; name=\"payload_json\"\r\n";
+            string cType = "Content-Type: application/octet-stream\r\n\r\n";
+            AddField(stream, bound, cDisposition, cType, Utils.Encode(json));
+        }
+
+        private void SetFile(MemoryStream stream, string bound, int index, FileInfo file)
+        {
+            string cDisposition = $"Content-Disposition: form-data; name=\"file_{index}\"; filename=\"{file.Name}\"\r\n";
+            string cType = "Content-Type: application/octet-stream\r\n\r\n";
+            AddField(stream, bound, cDisposition, cType, File.ReadAllBytes(file.FullName));
+        }
+
         /// <summary>
         /// Send webhook message
         /// </summary>
-        public void Send(DiscordMessage message, FileInfo file = null)
+        public void Send(DiscordMessage message, params FileInfo[] files)
         {
             if (string.IsNullOrEmpty(Url))
                 throw new ArgumentNullException("Invalid Webhook URL.");
@@ -23,31 +52,41 @@ namespace Discord.Webhook
             string bound = "------------------------" + DateTime.Now.Ticks.ToString("x");
             WebClient webhookRequest = new WebClient();
             webhookRequest.Headers.Add("Content-Type", "multipart/form-data; boundary=" + bound);
+            
             MemoryStream stream = new MemoryStream();
-            byte[] beginBodyBuffer = Encoding.UTF8.GetBytes("--" + bound + "\r\n");
-            stream.Write(beginBodyBuffer, 0, beginBodyBuffer.Length);
-            bool flag = file != null && file.Exists;
-            if (flag)
-            {
-                string fileBody = "Content-Disposition: form-data; name=\"file\"; filename=\"" + file.Name + "\"\r\nContent-Type: application/octet-stream\r\n\r\n";
-                byte[] fileBodyBuffer = Encoding.UTF8.GetBytes(fileBody);
-                stream.Write(fileBodyBuffer, 0, fileBodyBuffer.Length);
-                byte[] fileBuffer = File.ReadAllBytes(file.FullName);
-                stream.Write(fileBuffer, 0, fileBuffer.Length);
-                string fileBodyEnd = "\r\n--" + bound + "\r\n";
-                byte[] fileBodyEndBuffer = Encoding.UTF8.GetBytes(fileBodyEnd);
-                stream.Write(fileBodyEndBuffer, 0, fileBodyEndBuffer.Length);
-            }
-            string jsonBody = string.Concat(new string[]
-            {
-                "Content-Disposition: form-data; name=\"payload_json\"\r\nContent-Type: application/json\r\n\r\n",
-                string.Format("{0}\r\n", message),
-                "--",
-                bound,
-                "--"
-            });
-            byte[] jsonBodyBuffer = Encoding.UTF8.GetBytes(jsonBody);
-            stream.Write(jsonBodyBuffer, 0, jsonBodyBuffer.Length);
+            for (int i = 0; i < files.Length; i++)
+                SetFile(stream, bound, i, files[i]);
+
+            string json = message.ToString();
+            SetJsonPayload(stream, bound, json);
+
+            byte[] bodyEnd = Utils.Encode($"\r\n--{bound}--");
+            stream.Write(bodyEnd, 0, bodyEnd.Length);
+
+            //byte[] beginBodyBuffer = Encoding.UTF8.GetBytes("--" + bound + "\r\n");
+            //stream.Write(beginBodyBuffer, 0, beginBodyBuffer.Length);
+            //bool flag = file != null && file.Exists;
+            //if (flag)
+            //{
+            //    string fileBody = "Content-Disposition: form-data; name=\"file\"; filename=\"" + file.Name + "\"\r\nContent-Type: application/octet-stream\r\n\r\n";
+            //    byte[] fileBodyBuffer = Encoding.UTF8.GetBytes(fileBody);
+            //    stream.Write(fileBodyBuffer, 0, fileBodyBuffer.Length);
+            //    byte[] fileBuffer = File.ReadAllBytes(file.FullName);
+            //    stream.Write(fileBuffer, 0, fileBuffer.Length);
+            //    string fileBodyEnd = "\r\n--" + bound + "\r\n";
+            //    byte[] fileBodyEndBuffer = Encoding.UTF8.GetBytes(fileBodyEnd);
+            //    stream.Write(fileBodyEndBuffer, 0, fileBodyEndBuffer.Length);
+            //}
+            //string jsonBody = string.Concat(new string[]
+            //{
+            //    "Content-Disposition: form-data; name=\"payload_json\"\r\nContent-Type: application/json\r\n\r\n",
+            //    string.Format("{0}\r\n", message),
+            //    "--",
+            //    bound,
+            //    "--"
+            //});
+            //byte[] jsonBodyBuffer = Encoding.UTF8.GetBytes(jsonBody);
+            //stream.Write(jsonBodyBuffer, 0, jsonBodyBuffer.Length);
 
             try
             {
