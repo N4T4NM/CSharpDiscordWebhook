7ee5937dfc07007d9699a6a08c6e631ebb28a254
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 03:27:22 -0300

    Added fields name converter

---
+++ CSharpDiscordWebhook.NET/Discord/Json/DiscordJsonNaming.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+
+namespace CSharpDiscordWebhook.NET.Discord.Json
+{
+    public sealed class DiscordJsonNaming : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < name.Length; i++)
+            {
+                char c = name[i];
+                if (char.IsUpper(c) && i > 0)
+                    result += '_';
+
+                result += char.ToLower(c);
+            }
+
+            return result;
+        }
+    }
+}
