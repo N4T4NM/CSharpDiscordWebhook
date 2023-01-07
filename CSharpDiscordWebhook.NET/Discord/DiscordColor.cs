09859ea04aec2a48040fd620d5b27417f6557dd6
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 03:26:18 -0300

    Value classes to parse colors and timestamps

---
+++ CSharpDiscordWebhook.NET/Discord/DiscordColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace CSharpDiscordWebhook.NET.Discord
+{
+    public class DiscordColor
+    {
+        public Color Color { get; set; }
+
+        public DiscordColor(int color) : this(ColorTranslator.FromHtml(color.ToString("X6")))
+        {
+
+        }
+
+        public DiscordColor(Color color)
+        {
+            Color = color;
+        }
+
+        public int ToHexRgb()
+        {
+            string HS =
+            Color.R.ToString("X2") +
+            Color.G.ToString("X2") +
+            Color.B.ToString("X2");
+
+            return int.Parse(HS, System.Globalization.NumberStyles.HexNumber, null);
+        }
+    }
+}
---
+++ CSharpDiscordWebhook.NET/Discord/DiscordTimestamp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpDiscordWebhook.NET.Discord
+{
+    public class DiscordTimestamp
+    {
+        public DateTime Time { get; set; }
+
+        public DiscordTimestamp(string ts) : this(DateTime.Parse(ts))
+        {
+
+        }
+        public DiscordTimestamp(DateTime time)
+        {
+            Time = time;
+        }
+        public override string ToString() => Time.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz");
+    }
+}
