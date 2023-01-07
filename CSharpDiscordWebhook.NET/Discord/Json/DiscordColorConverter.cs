c59e09d9d93040618c014938ee4182efd567a9e9
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 03:27:06 -0300

    Added color and timestamp value converters

---
+++ CSharpDiscordWebhook.NET/Discord/Json/DiscordColorConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CSharpDiscordWebhook.NET.Discord.Json
+{
+    public sealed class DiscordColorConverter : JsonConverter<DiscordColor>
+    {
+        public override DiscordColor Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => new(reader.GetInt32());
+        public override void Write(Utf8JsonWriter writer, DiscordColor value, JsonSerializerOptions options)
+            => writer.WriteNumberValue(value.ToHexRgb());
+    }
+}
---
+++ CSharpDiscordWebhook.NET/Discord/Json/DiscordTimestampConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CSharpDiscordWebhook.NET.Discord.Json
+{
+    public class DiscordTimestampConverter : JsonConverter<DiscordTimestamp>
+    {
+        public override DiscordTimestamp Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string str = reader.GetString();
+            if (str == null) return null;
+            return new(str);
+        }
+        public override void Write(Utf8JsonWriter writer, DiscordTimestamp value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString());
+    }
+}
